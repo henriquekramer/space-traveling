7bf687e4f411b1a2d34d10581008779786d65b77
Henrique <henriquemkramer@gmail.com>
2022-05-13 18:34:39 -0300

    Front da Home finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from "react-icons/fi";
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,52 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar/>
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser/>
+                  Henrique Kramer
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Criando um app CRA do zero</strong>
+              <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+              <ul>
+                <li>
+                  <FiCalendar/>
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser/>
+                  Henrique Kramer
+                </li>
+              </ul>
+            </a>
+          </Link>
+          
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
